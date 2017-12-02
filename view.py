42bda55279c88794ff9aae81c65a4459c4938b04
Terence Martin <odatnurd@gmail.com>
2017-12-01 22:02:25 -0800

    Include support code for working with a help view
    This includes three key pieces of code that we will need to use in
    order to work with help.
    
    First is a function that will create a brand new view in a given
    window for use as help. This will set the tab text to an appropriate
    bit of text, make sure that the buffer is a scratch buffer, and have
    the appropriate syntax set.
    
    Second is a function that will scan a given window in order to find a
    previously created help view and return it back, so that its contents
    can be modified.
    
    Lastly is a function that uses the other two functions to display a
    help string in a help view, either creating a new one if it does not
    exist or reusing the existing one if it does.

---
+++ view.py
@@ -0,0 +1,68 @@
+import sublime
+
+
+_get_window = lambda wnd: wnd if wnd is not None else sublime.active_window()
+
+
+###----------------------------------------------------------------------------
+
+
+def find_help_view(window=None):
+    """
+    Search for and return the help view in the provided window. Defaults to
+    searching the current window if none is provided.
+    """
+    window = window if window is not None else sublime.active_window()
+    for view in window.views():
+        if view.name().startswith("HyperHelp"):
+            s = view.settings()
+            if s.has("_hh_pkg") and s.has("_hh_file"):
+                return view
+
+
+def new_help_view(syntax=None, window=None):
+    """
+    Create and return a new help view in the provided window. Defaults to the
+    current window if none is specified.
+    """
+    hView = _get_window(window).new_file()
+    hView.set_scratch(True)
+    hView.set_name("HyperHelp")
+
+    if syntax is not None:
+        hView.assign_syntax(syntax)
+
+    return hView
+
+
+def update_help_view(help_content, help_pkg, help_file,
+                     syntax=None, window=None):
+    """
+    Find or create the help view in the provided window and set it's contents
+    to the help string provided. The help view will have it's internal state
+    set up to track the given help package and file.
+    """
+    window = _get_window(window)
+    help_view = find_help_view(window)
+
+    if help_view is None:
+        help_view = new_help_view(syntax, window)
+    else:
+        help_view.set_read_only(False)
+        help_view.sel().clear()
+        help_view.sel().add(sublime.Region(0, help_view.size()))
+        help_view.run_command("left_delete")
+
+        if window.active_view() != help_view:
+            window.focus_view(help_view)
+
+    help_view.settings().set("_hh_pkg", help_pkg)
+    help_view.settings().set("_hh_file", help_file)
+
+    help_view.run_command("append", {"characters": help_content})
+    help_view.set_read_only(True)
+
+    return help_view
+
+
+###----------------------------------------------------------------------------
