ec40d6a0fda7a80d997edfb9a56002be5fb1fc4c
Terence Martin <odatnurd@gmail.com>
2017-12-02 20:26:52 -0800

    Implement a new method for displaying help
    This new method takes care of all of the details needed to get a help
    file displayed in a view.
    
    In particular any existing help view has its contents replaced with
    the new help, or a new help view will be created first. In both cases
    the help view is focused and the view specific settings that track
    what is being displayed are updated.
    
    In addition, these settings are now checked so that if the help file
    being asked for is already being displayed, nothing else happens since
    everything is already set up.
    
    The test command now uses this new method to display the sample help
    text.

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
 import sublime
 import sublime_plugin
 
-
-from .core import log, hh_syntax
-from .view import update_help_view
+from .core import log
+from .help import display_help
 
 ###----------------------------------------------------------------------------
 
 
 class HelpTestCommand(sublime_plugin.WindowCommand):
     def run(self):
-        help_file = "Packages/hyperhelp/help/sample.txt"
-        try:
-            help_txt = sublime.load_resource(help_file)
-        except:
-            help_txt = "Unable to load help text"
-
-        update_help_view(help_txt, "hyperhelp", "sample.txt",
-                         hh_syntax("HyperHelp.sublime-syntax"))
+        display_help("Packages/hyperhelp/help/sample.txt")
 
 
 ###----------------------------------------------------------------------------
