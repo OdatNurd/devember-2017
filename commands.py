d7898c0fc1b996920ddb1dae6f7f527bd01d413e
Terence Martin <odatnurd@gmail.com>
2017-12-03 21:52:05 -0800

    Include a command to navigate in the help
    The beginnings of the help navigation command are now in place. This
    command will take an argument that tells it where it should navigate
    to and then perform the navigation.
    
    The first navigation type included is the ability to jump through the
    document landing on the next or previous hidden anchor.

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 
+from .view import focus_on
 from .help import display_help
 
 
@@ -25,6 +26,35 @@ class HyperHelpReloadHelpCommand(sublime_plugin.TextCommand):
     def is_enabled(self):
         settings = self.view.settings()
         return settings.has("_hh_pkg") and settings.has("_hh_file")
+
+
+class HyperHelpNavigateCommand(sublime_plugin.TextCommand):
+    """
+    Perform navigation from within a help file
+    """
+    available_nav = ["find_anchor"]
+
+    def run(self, edit, nav, prev=False):
+        if nav == "find_anchor":
+            return self.anchor_nav(prev)
+
+    def is_enabled(self, nav, prev=False):
+        return nav in self.available_nav
+
+    def anchor_nav(self, prev):
+        anchors = self.view.settings().get("_hh_nav")
+        if not anchors:
+            return
+
+        point = self.view.sel()[0].begin()
+        fallback = anchors[-1] if prev else anchors[0]
+
+        pick = lambda p: (point < p[1][0]) if not prev else (point > p[1][0])
+        for pos in reversed(anchors) if prev else anchors:
+            if pick(pos):
+                return focus_on(self.view, pos[1])
+
+        focus_on(self.view, fallback[1])
 
 
 ###----------------------------------------------------------------------------
