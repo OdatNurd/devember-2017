6085a4723800f2b57254b3017166b7d903311ba1
Terence Martin <odatnurd@gmail.com>
2017-12-07 15:36:57 -0800

    Clean up the test file a little bit
    This does some slight refactoring in the test file so that we can
    retain previous tests if needed but still cleanly test new things.

--- test.py
+++ test.py
@@ -1,26 +1,37 @@
 import sublime
 import sublime_plugin
 
+from .common import log
+
 from .core import display_help
-# from .common import log
-# from .help_index import _load_help_index
+from .help_index import _load_help_index
+
+###----------------------------------------------------------------------------
+
+
+def _test_help():
+    display_help("Packages/hyperhelp/help/sample.txt")
+
+
+def _help_load_index():
+    info = _load_help_index("hyperhelp", "Packages/hyperhelp/help/hyperhelp.json")
+    if info:
+        from pprint import pformat
+
+        log("Loaded index for package: '%s'", info.package)
+        log("Index loaded from: '%s'", info.index_file)
+        log("Help description: '%s'", info.description)
+        log("Document root: '%s'", info.doc_root)
+        log("Available help topics:\n%s", pformat(info.help_topics))
+        log("Package TOC:\n%s", pformat(info.help_toc))
 
 ###----------------------------------------------------------------------------
 
 
 class HelpTestCommand(sublime_plugin.WindowCommand):
     def run(self):
-        display_help("Packages/hyperhelp/help/sample.txt")
-        # info = _load_help_index("hyperhelp", "Packages/hyperhelp/help/hyperhelp.json")
-        # if info:
-        #     from pprint import pformat
-
-        #     log("Loaded index for package: '%s'", info.package)
-        #     log("Index loaded from: '%s'", info.index_file)
-        #     log("Help description: '%s'", info.description)
-        #     log("Document root: '%s'", info.doc_root)
-        #     log("Available help topics:\n%s", pformat(info.help_topics))
-        #     log("Package TOC:\n%s", pformat(info.help_toc))
+        # _test_help()
+        _help_load_index()
 
 
 ###----------------------------------------------------------------------------
