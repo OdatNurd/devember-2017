7eb21ea66af9d1d8883a07ce50ca6b962f451951
Terence Martin <odatnurd@gmail.com>
2017-12-12 20:57:15 -0800

    Include a command to open an existing help file
    This adds a new command in the authoring tools that allows you to
    easily open a help file for editing.
    
    Like most authoring tool commands so far, this takes as parameters a
    package and a prompt argument and does the now-usual use the package
    or prompt for one thing.
    
    Once a package is selected (if needed), you see a list of files that
    exist in that help file's index and choosing one opens it for editing.
    
    This opens the file and forces the appropriate syntax onto the file,
    since it might not open with the correct syntax if it doesn't have an
    appropriate help header.
    
    Once that is done, a set of settings that is controlled via the
    settings file is applied to override the syntax specific settings, so
    that some aspects that you want disabled while viewing help are
    available while editing it.

--- authoring.py
+++ authoring.py
@@ -7,6 +7,7 @@ import re
 
 from .common import log, hh_syntax, current_help_package, help_package_prompt
 from .authoring_common import format_template, is_authoring_source
+from .authoring_common import local_help_filename, open_local_help
 from .core import help_index_list
 from .core import reload_help_file
 
@@ -61,7 +62,11 @@ class HyperhelpAuthorCreateHelp(sublime_plugin.WindowCommand):
             return help_package_prompt(help_index_list(),
                                        on_select=lambda pkg: self.run(pkg))
 
-        self.window.show_input_panel("New Help File", "",
+        if help_index_list().get(package, None) is None:
+            return log("Cannot add help file; package '%s' unknown", package,
+                       dialog=True)
+
+        self.window.show_input_panel("New Help File (%s)" % package, "",
                                      lambda file: self.create_file(package, file),
                                      None, None)
 
@@ -70,9 +75,7 @@ class HyperhelpAuthorCreateHelp(sublime_plugin.WindowCommand):
             return log("No help file given; skipping creation", status=True)
 
         pkg_info = help_index_list().get(package)
-        local_path = os.path.join(sublime.packages_path(),
-                                 pkg_info.doc_root,
-                                 file)
+        local_path = local_help_filename(pkg_info, file)
 
         help_file = os.path.split(local_path)
 
@@ -91,6 +94,39 @@ class HyperhelpAuthorCreateHelp(sublime_plugin.WindowCommand):
             """ % datetime.date.today().strftime("%Y-%m-%d"))
 
         view.run_command("insert_snippet", {"contents": template})
+
+
+class HyperhelpAuthorEditHelp(sublime_plugin.WindowCommand):
+    """
+    Open an existing help file from the package provided. If no package is
+    given and one cannot be inferred from the current help view, the user will
+    be prompted to supply one. The prompt always occurs if the argument asks.
+    """
+    def run(self, package=None, prompt=False):
+        package = package or current_help_package(window=self.window)
+        if package is None or prompt:
+            return help_package_prompt(help_index_list(),
+                                       on_select=lambda pkg: self.run(pkg))
+
+        pkg_info = help_index_list().get(package, None)
+        if pkg_info is None:
+            return log("Cannot edit help file; package '%s' unknown", package,
+                       dialog=True)
+
+        files = pkg_info.help_files
+        items = [[key, files[key]] for key in files]
+
+        if not items:
+            return log("The help index for '%s' lists no help files", package,
+                       dialog=True )
+
+        self.window.show_quick_panel(
+            items=items,
+            on_select=lambda i: self.edit_file(pkg_info, items, i))
+
+    def edit_file(self, pkg_info, items, index):
+        if index >= 0:
+            open_local_help(pkg_info, items[index][0], window=self.window)
 
 
 class HyperhelpAuthorReloadHelpCommand(sublime_plugin.TextCommand):
--- authoring_events.py
+++ authoring_events.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 
 from .authoring_common import hha_setting, is_authoring_source
+from .authoring_common import apply_authoring_settings
 
 
 ###----------------------------------------------------------------------------
@@ -15,6 +16,16 @@ class HyperhelpAuthorEventListener(sublime_plugin.EventListener):
         """
         if hha_setting("update_header_on_save") and is_authoring_source(view):
             view.run_command("hyperhelp_author_update_header", {"quiet": True})
+
+    def on_load(self, view):
+        """
+        If the view is a help file that is marked as being opened by the
+        authoring tool, apply the appropriate authoring settings to it to make
+        editing easier.
+        """
+        if view.settings().get("_hh_auth", None) is not None:
+            view.settings().erase("_hh_auth")
+            apply_authoring_settings(view)
 
 
 ###----------------------------------------------------------------------------
