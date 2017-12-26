fde27b3cb3aac443e81a7b4f24f1528d29832372
Terence Martin <odatnurd@gmail.com>
2017-12-25 21:21:18 -0800

    Stage one of a big lint refactor
    This pulls a lot of the linting code out of the lint command itself
    and puts it into helper routines where it belongs.
    
    This is just the first part of this operation, which aims to remove
    all of the code not directly related to performing the lint so that
    more lint types can be added.

--- HyperHelpAuthor/linter.py
+++ HyperHelpAuthor/linter.py
@@ -2,7 +2,7 @@ import sublime
 import sublime_plugin
 
 import os
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 
 from hyperhelp.common import log, hh_syntax
 from hyperhelp.core import help_index_list, lookup_help_topic
@@ -11,68 +11,129 @@ from hyperhelp.core import help_index_list, lookup_help_topic
 ###----------------------------------------------------------------------------
 
 
-class HyperhelpAuthorLint(sublime_plugin.WindowCommand):
-    def run(self):
-        pkg_info, lint_type, targets = self.get_lint_targets()
-        if pkg_info is None:
-            return log("Unable to lint; package is not in help index",
-                       status=True)
+# A representation of what is going to be linted.
+LintTarget = namedtuple("LintTarget", [
+    "target_type", "pkg_info", "files"
+])
 
-        output = []
-        output.append("Linting Help Package: %s\n" % pkg_info.package)
-        for target in targets:
-            self.lint_target(pkg_info, target, output)
 
-        self.display_output(pkg_info, output)
+###----------------------------------------------------------------------------
 
-    def is_enabled(self):
-        view = self.window.active_view()
-        if (view.match_selector(0, "text.hyperhelp") and
-                view.is_read_only() == False and
-                view.file_name() is not None):
-            return view.file_name().startswith(sublime.packages_path())
 
-        return False
+def _can_lint_view(view):
+    """
+    Determine if the provided view can be the source of a lint. To be valid
+    the view must represent a hyperhelp data file that has a path rooted in the
+    Packages folder inside of a package whose help index is known.
+    """
+    if (view is not None and view.file_name() is not None and
+            view.file_name().startswith(sublime.packages_path()) and
+            view.match_selector(0, "text.hyperhelp")):
 
-    def get_lint_targets(self):
-        view = self.window.active_view()
         name = os.path.relpath(view.file_name(), sublime.packages_path())
-        parts = name.split(os.sep)
+        pkg_name = name[:name.index(os.sep)]
+        return pkg_name in help_index_list()
 
-        pkg_name = parts[0]
-        target = parts[-1]
+    return False
 
-        pkg_info = help_index_list().get(pkg_name, None)
-        if pkg_info is None:
-            return (None, None, None)
 
-        if view.match_selector(0, "text.hyperhelp.help"):
-            return (pkg_info, "single", [target])
-
-        return (pkg_info, "package", list(pkg_info.help_files))
-
-    def get_temp_view(self, filename):
-        content = None
-        try:
-            with open(filename, 'r') as file:
-                content = file.read()
-        except:
-            pass
-
-        if content:
-            view = self.window.create_output_panel("_hh_tmp", True)
-            view.run_command("select_all")
-            view.run_command("left_delete")
-            view.run_command("append", {"characters": content})
-            view.assign_syntax(hh_syntax("HyperHelp.sublime-syntax"))
-            return view
-
+def _find_lint_target(view):
+    """
+    Examine a given view and return a LintTarget that describes what is being
+    linted. None is returned if the view is not a valid lint target.
+    """
+    if not _can_lint_view(view):
         return None
 
-    def lint_target(self, pkg_info, target, output):
+    name = view.file_name()
+    parts = os.path.relpath(name, sublime.packages_path()).split(os.sep)
+
+    pkg_name = parts[0]
+    target = parts[-1]
+
+    pkg_info = help_index_list().get(pkg_name)
+
+    if view.match_selector(0, "text.hyperhelp.help"):
+        return LintTarget("single", pkg_info, [target])
+
+    return LintTarget("package", pkg_info, list(pkg_info.help_files))
+
+
+def _get_lint_file(filename):
+    """
+    Return a view that that contains the contents of the provided file name.
+    If the file is not aready loaded, it is loaded into a hidden view and that
+    is returned instead.
+
+    Can return None if the file is not open and cannot be loaded.
+    """
+    for window in sublime.windows():
+        view = window.find_open_file(filename)
+        if view is not None:
+            return view
+
+    content = None
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+    except:
+        pass
+
+    if content:
+        view = sublime.active_window().create_output_panel("_hha_tmp", True)
+        view.run_command("select_all")
+        view.run_command("left_delete")
+        view.run_command("append", {"characters": content})
+        view.assign_syntax(hh_syntax("HyperHelp.sublime-syntax"))
+        return view
+
+    return None
+
+
+def _display_lint(window, pkg_info, output):
+    """
+    Display the lint output provided into the given window. The output is
+    assumed to have been generated from the provided package, which is used to
+    know where the help files are located.
+    """
+    view = window.create_output_panel("HyperHelpAuthor Lint", False)
+    basedir = os.path.join(sublime.packages_path(), pkg_info.doc_root)
+
+    if not isinstance(output, str):
+        output = "\n".join(output)
+
+    settings = view.settings()
+    settings.set("result_base_dir", basedir)
+    settings.set("result_file_regex", r"^\s+([^:]+):(\d+):(\d+): (.*)$")
+
+    view.set_read_only(False)
+    view.run_command("select_all")
+    view.run_command("delete_left")
+    view.run_command("append", {"characters": output})
+    view.set_read_only(True)
+
+    window.run_command("show_panel", {"panel": "output.HyperHelpAuthor Lint"})
+
+
+###----------------------------------------------------------------------------
+
+
+class HyperhelpAuthorLint(sublime_plugin.WindowCommand):
+    def run(self):
+        lint = _find_lint_target(self.window.active_view())
+        output = ["Linting package: %s\n" % lint.pkg_info.package]
+
+        for file in lint.files:
+            self.lint_file(lint.pkg_info, file, output)
+
+        _display_lint(self.window, lint.pkg_info, output)
+
+    def is_enabled(self):
+        return _can_lint_view(self.window.active_view())
+
+    def lint_file(self, pkg_info, target, output):
         name = os.path.join(sublime.packages_path(), pkg_info.doc_root, target)
-        view = self.window.find_open_file(name)
-        view = view or self.get_temp_view(name)
+        view = _get_lint_file(name)
         if view is None:
             return log("Unable to lint '%s' in '%s'", target, pkg_info.package,
                        status=True)
@@ -109,21 +170,5 @@ class HyperhelpAuthorLint(sublime_plugin.WindowCommand):
             count, "" if count == 1 else "s"))
         output.append("")
 
-
-    def display_output(self, pkg_info, output):
-        lint_view = self.window.create_output_panel("HyperHelp Lint", False)
-        s = lint_view.settings()
-
-        base = os.path.join(sublime.packages_path(), pkg_info.doc_root)
-        s.set("result_base_dir", base)
-        s.set("result_file_regex", r"^\s+([^:]+):(\d+):(\d+): (.*)$")
-
-        lint_view.set_read_only(False)
-        lint_view.run_command("select_all")
-        lint_view.run_command("delete_left")
-        lint_view.run_command("append", {"characters": "\n".join(output)})
-        lint_view.set_read_only(True)
-
-        self.window.run_command("show_panel", {"panel": "output.HyperHelp Lint"})
 
 ###----------------------------------------------------------------------------
